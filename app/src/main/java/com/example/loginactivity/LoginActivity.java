4c26ba83a7aa368bded3833472893ea976f540e2
Aditya Kumar <69967445+Aditya-043@users.noreply.github.com>
2023-03-29 08:58:37 +0530

    my my commit

---
+++ app/src/main/java/com/example/loginactivity/LoginActivity.java
@@ -0,0 +1,44 @@
+package com.example.loginactivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText usernameEditText, passwordEditText;
+    private Button loginButton;
+    private TextView errorMessageTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        usernameEditText = findViewById(R.id.usernameEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        loginButton = findViewById(R.id.loginButton);
+        errorMessageTextView = findViewById(R.id.errorMessageTextView);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                if (username.equals("admin") && password.equals("password")) {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    errorMessageTextView.setText("Invalid username or password.");
+                }
+            }
+        });
+    }
+}
